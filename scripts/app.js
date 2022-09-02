bdc274d80f688ca50014cb3bc700630ccd8ac801
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-09-03 05:51:47 +0600

    phone hunter added

---
+++ scripts/app.js
@@ -0,0 +1,93 @@
+const loadPhones = async(searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+}
+
+const displayPhones = (phones, dataLimit) => {
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
+
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+        phones = phones.slice(0,10);
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
+
+    const notFoundMSG = document.getElementById('not-found-msg');
+    if(phones.length === 0){
+        notFoundMSG.classList.remove('d-none')
+    }
+    else{
+        notFoundMSG.classList.add('d-none')
+    }
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-3">
+            <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-center">${phone.phone_name}</h5>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="mt-3 d-block mx-auto btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+    toggleLoader(false);
+}
+
+const processSearch = (dataLimit) => {
+    toggleLoader(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+document.getElementById('btn-search').addEventListener('click',function(){
+    processSearch(10);
+});
+
+document.getElementById('search-field').addEventListener('keypress',function(event){
+    if (event.key === 'Enter'){
+        processSearch(10);
+    }
+});
+
+const toggleLoader = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    processSearch();
+})
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    <p> Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+    <p> Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Information Found'}</p>
+    <p> Others: ${phone.others ? phone.others.Bluetooth : 'No Information Found'}</p>
+    `
+}
